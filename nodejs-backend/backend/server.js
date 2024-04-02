f2c49a552beb53ea70d597a53026b480572fe6a6
R4Tobi <R4Tobi@users.noreply.github.com>
2024-04-02 20:44:32 +0200

    added test, login and health

--- nodejs-backend/backend/server.js
+++ nodejs-backend/backend/server.js
@@ -1,16 +1,39 @@
-const http = require("node:http");
+var express = require("express");
+var path = require("path");
+const { MongoClient } = require("mongodb");
 
-const hostname = "0.0.0.0";
+const bodyParser = require('body-parser'); // Middleware
 
-const port = 8080;
+var client = new MongoClient("mongodb://root:password@localhost:27017");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
+app = express();
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  console.log(`${req.method}: ${req.ip} ${req.hostname}, ${req.protocol}, ${req.path}`);
+  next();
 });
 
+app.get("/health", (req, res) => {
+  res.send("{ success: true, message: \"Everything is working fine\"}");
+});
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+});
+
+app.post("/test", (req, res) => {
+  // Connect to database
+  client
+    .connect()
+    .then(() => console.log("Connected Successfully"))
+    .catch((error) => console.log("Failed to connect", error));
+})
+
+var port = 8080;
+var hostname = "0.0.0.0";
+
+app.listen(port, hostname, () => {
+  console.log(`Server running on port ${port}`);
 });
