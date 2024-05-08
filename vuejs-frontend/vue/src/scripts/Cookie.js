c6fddb7934fb682112f4c5f4ccf84eb2c808e0af
Tobias Baake <62656419+R4Tobi@users.noreply.github.com>
2024-05-08 22:57:40 +0200

    added deleteCookie

--- vuejs-frontend/vue/src/scripts/Cookie.js
+++ vuejs-frontend/vue/src/scripts/Cookie.js
@@ -28,6 +28,10 @@ class Cookie {
     }
     return "";
   }
+
+  deleteCookie(name) {
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  }
 }
 
 export default Cookie;
