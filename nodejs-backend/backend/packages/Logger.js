27e489f75ae13cfe0a7f97aa56d5ac74f911bea1
Tobias Baake <62656419+R4Tobi@users.noreply.github.com>
2024-05-13 11:51:01 +0200

    updated Logger.js

--- nodejs-backend/backend/packages/Logger.js
+++ nodejs-backend/backend/packages/Logger.js
@@ -19,90 +19,96 @@ class Logging {
 
   constructor(writeFile) {
     this.writeFile = writeFile;
-    fs.mkdir("./logs", { recursive: true }, (err) => {
-      if (err) throw err;
-    });
-    fs.writeFile("./logs/info.log", "", function (err) {
-      if (err) throw err;
-    });
-    fs.writeFile("./logs/warn.log", "", function (err) {
-      if (err) throw err;
-    });
-    fs.writeFile("./logs/error.log", "", function (err) {
-      if (err) throw err;
-    });
-    fs.writeFile("./logs/dev.log", "", function (err) {
-      if (err) throw err;
-    });
-    fs.writeFile("./logs/session.log", "", function (err) {
-      if (err) throw err;
-    });
+    try {
+      fs.mkdirSync("./logs", { recursive: true }, (err) => {
+        if (err) throw err;
+      });
+      fs.writeFile("./logs/info.log", "", function (err) {
+        if (err) throw err;
+      });
+      fs.writeFile("./logs/warn.log", "", function (err) {
+        if (err) throw err;
+      });
+      fs.writeFile("./logs/error.log", "", function (err) {
+        if (err) throw err;
+      });
+      fs.writeFile("./logs/dev.log", "", function (err) {
+        if (err) throw err;
+      });
+      fs.writeFile("./logs/session.log", "", function (err) {
+        if (err) throw err;
+      });
+    } catch (e) {}
   }
 
-  getTimestamp(){
-      const date = new Date();
-      const day = String(date.getDate()).padStart(2, '0');
-      const month = String(date.getMonth() + 1).padStart(2, '0');
-      const year = date.getFullYear();
-      const hours = String(date.getHours()).padStart(2, '0');
-      const minutes = String(date.getMinutes()).padStart(2, '0');
-      const seconds = String(date.getSeconds()).padStart(2, '0');
-      const milliseconds = String(date.getMilliseconds()).padStart(3, '0');
-      return `[${day}.${month}.${year} ${hours}:${minutes}:${seconds},${milliseconds}]`;
-  };
+  getTimestamp() {
+    const date = new Date();
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    const milliseconds = String(date.getMilliseconds()).padStart(3, "0");
+    return `[${day}.${month}.${year} ${hours}:${minutes}:${seconds},${milliseconds}]`;
+  }
 
-  writeLog(message, filename){
-    fs.appendFile("./logs/" + filename, this.getTimestamp() + " " + message + "\n", function (err) {
-      if (err) throw err;
-    });
-  };
+  writeLog(message, filename) {
+    fs.appendFile(
+      "./logs/" + filename,
+      this.getTimestamp() + " " + message + "\n",
+      function (err) {
+        if (err) throw err;
+      }
+    );
+  }
 
   //Logging
   query(message) {
     console.log(this.lightblue + "QUERY   " + this.neutral + message);
-    if(this.writeFile){
+    if (this.writeFile) {
       this.writeLog("QUERY   " + message, "info.log");
     }
   }
   warn(message) {
     console.log(this.yellow + "WARN    " + this.neutral + message);
-    if(this.writeFile){
+    if (this.writeFile) {
       this.writeLog("WARN " + message, "warn.log");
     }
   }
   error(message) {
     console.log(this.red + "ERROR   " + this.neutral + message);
-    if(this.writeFile){
+    if (this.writeFile) {
       this.writeLog("ERROR " + message, "error.log");
     }
   }
   success(message) {
     console.log(this.green + "SUCCESS " + this.neutral + message);
-    if(this.writeFile){
+    if (this.writeFile) {
       this.writeLog("SUCCESS " + message, "info.log");
     }
   }
   info(message) {
     console.log(this.blue + "INFO    " + this.neutral + message);
-    if(this.writeFile){
+    if (this.writeFile) {
       this.writeLog("INFO    " + message, "info.log");
     }
   }
   dev(message) {
     console.log(this.magenta + "DEV     " + this.neutral + message);
-    if(this.writeFile){
+    if (this.writeFile) {
       this.writeLog("DEV " + message, "dev.log");
     }
   }
   session(message) {
     console.log(this.lightblue + "SESSION " + this.neutral + message);
-    if(this.writeFile){
+    if (this.writeFile) {
       this.writeLog("SESSION " + message, "session.log");
     }
   }
   register(message) {
     console.log(this.green + "REGISTER" + this.neutral + message);
-    if(this.writeFile){
+    if (this.writeFile) {
       this.writeLog("REGISTER" + message, "info.log");
     }
   }
